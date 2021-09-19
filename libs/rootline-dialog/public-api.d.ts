344d6faea79ca1caa0d2323f076bbe442c13cb88
EkhlasMridha <41969194+EkhlasMridha@users.noreply.github.com>
2021-09-19 19:02:46 +0600

    junk file cleaned

--- libs/rootline-dialog/public-api.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/rootline-dialog.service';
-export * from './lib/rootline-dialog/rootline-dialog.component';
-export * from './lib/rootline-dialog.module';
-//# sourceMappingURL=public-api.d.ts.map
